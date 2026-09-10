323a3ed5de09c674e687f067867b56983918e8b1
Stephen Joseph <81881979+Smartdevs17@users.noreply.github.com>
2026-09-10 14:30:17 +0100

    feat(scraper): export scraper package surface

---
+++ packages/scraper/src/index.ts
@@ -0,0 +1,4 @@
+export { scrapePromenade, promenadeDefaultConfig } from './promenade.js';
+export { discoverPromotionLinks, parsePromotionDetail, parseBrandDetail } from './parser.js';
+export { canonicalizeUrl, makeSourceKey, normalizeBrandName, normalizeWhitespace } from './normalize.js';
+export type { ScrapedBrand, ScrapedPromotion, ScrapeDiagnostics, ScrapeResult, ScraperConfig } from './types.js';
